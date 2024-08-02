e8b0892b4f0c958fe40fd95a5aca477174d51941
Al-Abir <sheikhabir196@gmail.com>
2024-08-02 11:00:17 +0600

    code

---
+++ Loop/for.js
@@ -0,0 +1,44 @@
+var prompt = require('prompt-sync')();
+
+
+let sum =0;
+
+let a = prompt("Please Input Your Number: ");
+  
+
+a = Number.parseInt(a);
+
+console.log(typeof a);
+
+
+for(let i=0; i<a; i++){
+    sum+= i;
+}
+
+console.log(sum);
+
+
+// for in loop
+
+let obj = {
+    abir:4,
+    nahiyan:5,
+    tosher:6,
+}
+
+for(let access in obj){
+    console.log(access);  //ouput key
+}
+for(let access in obj){
+    console.log(obj[access]); //only value
+}
+for(let access in obj){
+    console.log(" key " + access + " value " + obj[access]); //key and value
+}
+
+//for of loop
+
+for(let letter of "abir"){
+  console.log(letter);
+}
+// Note string and  iterable hot hobe
--- Loop/hi.js
+++
@@ -1,9 +0,0 @@
-var prompt = require('prompt-sync')();
-
-let age = prompt("What is your age = ");
-if(age>10 && age<=20){
-    console.log("You are adult")
-}
-else{
-    console.log("You are child");
-}
